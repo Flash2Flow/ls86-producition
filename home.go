7e6312ebd07f6145b3b3147785e6d4764b48e9f3
unknown <re-incarnation>
2022-07-15 00:39:51 +1000

    refract lk/add check cookie func

--- home.go
+++ home.go
@@ -72,105 +72,47 @@ func auth(w http.ResponseWriter, r *http.Request) {
 }
 
 func lkuser(w http.ResponseWriter, r *http.Request) {
-	tokenCookie, err := r.Cookie("hash")
-	if err == nil {
-		id, err2 := r.Cookie("ID")
-		if err2 == nil {
-			if tokenCookie.Value != " " {
-				if id.Value != " " {
-					u, err := data.FindOne("id", id.Value)
-					switch err {
-					case customErr.ErrNotFound:
-						//err not found
-						log.Println(customErr.ErrNotFound)
+	boo, u := checkCookie(r)
+	if boo == true {
 
-					case nil:
-						if tokenCookie.Value == u.AuthToken {
+		temp, err := template.ParseFiles("temp/lk_user.html", "static/lk_user/src/app.js", "static/lk_user/src/jquery-3.6.0.min.js")
 
-							temp, err := template.ParseFiles("temp/lk_user.html", "static/lk_user/src/app.js", "static/lk_user/src/jquery-3.6.0.min.js")
-
-							if err != nil {
-								fmt.Fprintf(w, err.Error())
-							}
-
-							temp.ExecuteTemplate(w, "lk_user", u)
-						} else {
-							//bad token
-							log.Println("bad token")
-						}
-					default:
-						//err unknown
-						log.Println(err)
-					}
-
-				} else {
-					//empty id
-					log.Println("empty id")
-				}
-			} else {
-				//empty token
-				log.Println("empty token")
-			}
-		} else {
-			//err cookie id
-			log.Println("err cookie id")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
 		}
+
+		temp.ExecuteTemplate(w, "lk_user", u)
 	} else {
-		//err cookie token
-		log.Println("err cookie token")
+		log.Println("LK ERROR")
+		temp, err := template.ParseFiles("temp/redirects/toHome.html")
+
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		temp.ExecuteTemplate(w, "redirect_main", nil)
 	}
 
 }
 
 func lk(w http.ResponseWriter, r *http.Request) {
-	tokenCookie, err := r.Cookie("hash")
-	if err == nil {
-		id, err2 := r.Cookie("ID")
-		if err2 == nil {
-			if tokenCookie.Value != " " {
-				if id.Value != " " {
-					u, err := data.FindOne("id", id.Value)
-					switch err {
-					case customErr.ErrNotFound:
-						//err not found
-						log.Println(customErr.ErrNotFound)
+	boo, u := checkCookie(r)
+	if boo == true {
+		temp, err := template.ParseFiles("temp/lk.html", "static/lk/src/app.js", "static/lk/src/jquery-3.6.0.min.js")
 
-					case nil:
-						if tokenCookie.Value == u.AuthToken {
-
-							temp, err := template.ParseFiles("temp/lk.html", "static/lk/src/app.js", "static/lk/src/jquery-3.6.0.min.js")
-
-							if err != nil {
-								fmt.Fprintf(w, err.Error())
-							}
-
-							temp.ExecuteTemplate(w, "lk", u)
-						} else {
-							//bad token
-							log.Println("bad token")
-						}
-					default:
-						//err unknown
-						log.Println(err)
-					}
-
-				} else {
-					//empty id
-					log.Println("empty id")
-				}
-			} else {
-				//empty token
-				log.Println("empty token")
-			}
-		} else {
-			//err cookie id
-			log.Println("err cookie id")
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
 		}
-	} else {
-		//err cookie token
-		log.Println("err cookie token")
-	}
 
+		temp.ExecuteTemplate(w, "lk", u)
+	} else {
+		log.Println("LK ERROR")
+		temp, err := template.ParseFiles("temp/redirects/toHome.html")
+
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+		}
+		temp.ExecuteTemplate(w, "redirect_main", nil)
+	}
 }
 
 func exit(w http.ResponseWriter, r *http.Request) {
@@ -201,4 +143,56 @@ func exit(w http.ResponseWriter, r *http.Request) {
 	}
 
 	temp.ExecuteTemplate(w, "redirect_main", nil)
+}
+
+func checkCookie(r *http.Request) (bool, *User) {
+	tokenCookie, err := r.Cookie("hash")
+	if err == nil {
+		id, err2 := r.Cookie("ID")
+		if err2 == nil {
+			if tokenCookie.Value != " " {
+				if id.Value != " " {
+					u, err := data.FindOne("id", id.Value)
+					switch err {
+					case customErr.ErrNotFound:
+						//err not found
+						log.Println(customErr.ErrNotFound)
+						return false, nil
+
+					case nil:
+						if tokenCookie.Value == u.AuthToken {
+
+							return true, u
+						} else {
+							//bad token
+							log.Println("bad token")
+							return false, nil
+						}
+					default:
+						//err unknown
+						log.Println(err)
+						return false, nil
+					}
+
+				} else {
+					//empty id
+					log.Println("empty id")
+					return false, nil
+				}
+			} else {
+				//empty token
+				log.Println("empty token")
+				return false, nil
+			}
+		} else {
+			//err cookie id
+			log.Println("err cookie id")
+			return false, nil
+		}
+	} else {
+		//err cookie token
+		log.Println("err cookie token")
+		return false, nil
+	}
+	return false, nil
 }
---
+++ ucp.go
@@ -0,0 +1,7 @@
+package main
+
+import "net/http"
+
+func ucp(w http.ResponseWriter, r *http.Request) {
+
+}
